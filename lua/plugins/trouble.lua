34c88896391b0594a8be6cee6f6ec4bdbdf9bbd8
Konstantinos Papazafeiropoulos <kostas.papazafeiropoulos@tnp.eu>
2025-10-08 10:19:59 +0100

    chore: adding diagnostics preview window

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -35,15 +35,14 @@ return {
   },
   opts = {
     modes = {
-      preview_float = {
-        mode = 'diagnostics',
+      diagnostics = {
         preview = {
           type = 'float',
           relative = 'editor',
           border = 'rounded',
           title = 'Preview',
           title_pos = 'center',
-          position = { 0, -2 },
+          position = { -1, -2 },
           size = { width = 0.3, height = 0.3 },
           zindex = 200,
         },
