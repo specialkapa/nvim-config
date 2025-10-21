db134540894546a2ca7d9448156ee965f6e3e2c4
Konstantinos Papazafeiropoulos <kostas.papazafeiropoulos@tnp.eu>
2025-10-21 22:08:03 +0100

    refactor: gather all git related plugins to a single lua module

---
+++ lua/plugins/git.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    -- Powerful Git integration for Vim
+    'tpope/vim-fugitive',
+  },
+  {
+    -- GitHub integration for vim-fugitive
+    'tpope/vim-rhubarb',
+  },
+  {
+    'f-person/git-blame.nvim',
+    lazy = true,
+    opts = {
+      enabled = true,
+      message_template = ' <summary>, <date>, <author>, <<sha>>',
+      date_format = '%Y-%m-%d %H:%M:%S',
+      display_virtual_text = 0,
+      use_blame_commit_file_urls = true,
+    },
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      signs_staged = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}
