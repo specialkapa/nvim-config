60bd90297017ad76439c1b5361f898f2241fd301
Konstantinos Papazafeiropoulos <kostas.papazafeiropoulos@tnp.eu>
2025-10-30 09:14:41 +0000

    chore: adding a second vertical ruler on the 100th column

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,4 @@
-vim.opt.colorcolumn = '79' -- Add ruler at 79th column
+vim.opt.colorcolumn = '79,100' -- Add ruler at 79th and 100th columns
 vim.wo.number = true -- Make line numbers default (default: false)
 vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim. (default: '')
 vim.o.wrap = false -- Display lines as one long line (default: true)
