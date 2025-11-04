c00d12dd51939708faf7c5cbf0bced656d0bb5ef
Konstantinos Papazafeiropoulos <kostas.papazafeiropoulos@tnp.eu>
2025-11-04 18:04:44 +0000

    chore: adding some more robustness in the choice of python binary

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -89,8 +89,13 @@ return {
           program = '${file}', -- This configuration will launch the current file if used.
           pythonPath = function()
             -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            -- The code below first respects `VIRTUAL_ENV`, then looks for `venv` or `.venv` folders in the current directory.
+            local python_bin = vim.env.VIRTUAL_ENV
+            if python_bin and python_bin ~= '' then
+              if vim.fn.isdirectory(python_bin) == 1 and vim.fn.executable(python_bin) == 1 then
+                return python_bin
+              end
+            end
             local cwd = vim.fn.getcwd()
             if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
               return cwd .. '/venv/bin/python'
